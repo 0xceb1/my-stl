4fbc0ad08671e37a0c3de3707ea118fb3336846f
Darren Weng <wjhdarren@gmail.com>
2025-12-30 23:33:05 +0800

    Add tests for ctors of my::optional

--- Makefile
+++ Makefile
@@ -1,10 +1,22 @@
-CXX = clang++
-CXXFLAGS = -std=c++23 -Wall -Wextra
-INCLUDES = -I. -I/opt/homebrew/include
-LDFLAGS = -L/opt/homebrew/lib
+CXX       := clang++
+CXXFLAGS  := -std=c++23 -Wall -Wextra -I. -I/opt/homebrew/include
+LDFLAGS   := -L/opt/homebrew/lib
 
-main: main.cpp
-	$(CXX) $(CXXFLAGS) $(INCLUDES) -o main main.cpp $(LDFLAGS)
+LIB_DIR   := my
+TEST_DIR  := tests
+BUILD_DIR := build
+BIN_DIR   := bin
+
+TEST_SRCS := $(wildcard $(TEST_DIR)/*.cpp)
+LIB_SRCS  := $(wildcard $(LIB_DIR)/*.hpp)
+
+main: main.cpp $(LIB_SRCS)
+	@mkdir -p $(BIN_DIR)
+	$(CXX) $(CXXFLAGS) -o $(BIN_DIR)/main main.cpp $(LDFLAGS)
+
+test: $(TEST_SRCS)
+	@mkdir -p $(BIN_DIR)
+	$(CXX) $(CXXFLAGS) $(TEST_SRCS) -o $(BIN_DIR)/run_tests $(LDFLAGS) -lCatch2 -lCatch2Main
 
 clean:
-	rm -f main
+	rm -rf $(BIN_DIR)
---
+++ tests/optional_test.cpp
@@ -0,0 +1,54 @@
+#include <optional>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <utility>
+#include <catch2/catch_test_macros.hpp>
+#include "my/optional.hpp"
+
+using std::optional;
+using std::string;
+using std::vector;
+using MaybeStr = optional<string>;
+using MaybeCstr = optional<const char *>;
+using MaybeVec = optional<vector<int>>;
+
+TEST_CASE("my::optional can be constructed", "[my::optional]") {
+    MaybeStr none {};
+    MaybeStr some { "hello" };
+    MaybeCstr some_cstr { "world" };
+    optional<MaybeStr> some_none = MaybeStr {};
+
+    SECTION("default constructed") {
+        REQUIRE_FALSE(none);
+    }
+
+    SECTION("constructed from content") {
+        REQUIRE(some.value() == "hello");
+        REQUIRE(some_none);
+        REQUIRE_FALSE(some_none.value());
+    }
+
+    SECTION("copy constructed") {
+        MaybeStr copied_some{ some };
+        MaybeStr forward_copied{ some_cstr };
+        REQUIRE(some.value() == "hello");
+        REQUIRE(copied_some.value() == "hello");
+        REQUIRE(std::strcmp(some_cstr.value(), "world") == 0);
+        REQUIRE(forward_copied.value() == "world");
+    }
+
+    SECTION("move constructed") {
+        // TODO: forward move
+        MaybeStr moved_some{ std::move(some) };
+        REQUIRE(some);
+        REQUIRE(some.value() == "");
+        REQUIRE(moved_some.value() == "hello");
+    }
+
+    SECTION("construced from content in place & ilist") {
+        MaybeVec some_vec(std::in_place, {1, 2, 3, 4, 5});
+        REQUIRE(some_vec.value().size() == 5);
+    }
+}
+
